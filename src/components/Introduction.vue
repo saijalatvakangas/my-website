d65006bcc1dae537b1d945ac07b2bceb09dde104
Saija Latvakangas <saijalatvakangas@gmail.com>
2020-09-06 12:35:09 +0300

    added some design to skills and introduction

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card class="mt-16 py-16" min-height="80vh">
-    <h1>Esittely</h1>
+    <v-card-title class="display-1 d-flex justify-center">Esittely</v-card-title>
   </v-card>
 </template>
 
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,28 +1,17 @@
 <template>
   <v-card class="mt-16 py-16" min-height="80vh">
-    <v-card-title>Osaaminen</v-card-title>
+    <v-card-title class="display-1 d-flex justify-center">Osaaminen</v-card-title>
     <v-row no-gutters>
-      <v-col
-        v-for="(skill, i) in skills" :key="i"
-        cols="12"
-        sm="6"
-        md="4"
-      >
-      <v-card  color="#385F73" dark class="ma-2">
-        <v-card-title class="headline">
-          <v-icon large>
-            {{skill.logo}}
-          </v-icon>
-        </v-card-title>
+      <v-col v-for="(skill, i) in skills" :key="i" cols="12" md="4" sm="6">
+        <v-card elevation="0" class="ma-2">
+          <v-card-title>
+            <v-icon large class="py-3" color="deep-purple lighten-1">{{skill.logo}}</v-icon>
+          </v-card-title>
 
-        <v-card-subtitle class="headline font-weight-bold">{{skill.headline}}</v-card-subtitle>
+          <v-card-subtitle class="headline font-weight-medium black--text">{{skill.headline}}</v-card-subtitle>
 
-        <v-card-text class="font-weight-bold">
-          {{skill.description}}
-        </v-card-text>
-
-      </v-card>
-
+          <v-card-text class="font-weight-bold">{{skill.description}}</v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-card>
@@ -35,37 +24,44 @@ export default {
       {
         logo: "mdi-twitter",
         headline: "Twitter",
-        description: "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
+        description:
+          "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
       },
       {
         logo: "mdi-twitter",
         headline: "Twitter",
-        description: "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
+        description:
+          "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
       },
       {
         logo: "mdi-twitter",
         headline: "Twitter",
-        description: "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
+        description:
+          "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
       },
       {
         logo: "mdi-twitter",
         headline: "Twitter",
-        description: "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
+        description:
+          "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
       },
       {
         logo: "mdi-twitter",
         headline: "Twitter",
-        description: "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
+        description:
+          "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
       },
       {
         logo: "mdi-twitter",
         headline: "Twitter",
-        description: "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
+        description:
+          "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
       },
       {
         logo: "mdi-twitter",
         headline: "Twitter",
-        description: "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
+        description:
+          "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well.",
       },
     ],
   }),
