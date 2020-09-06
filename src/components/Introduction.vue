cec3202da5b21737d9443def543f5a13978d5507
Saija Latvakangas <saijalatvakangas@gmail.com>
2020-09-06 11:17:38 +0300

    added outline for introduction card

---
+++ src/components/Introduction.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card class="mt-16 py-16" min-height="80vh">
+    <h1>Esittely</h1>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    //
+  }),
+};
+</script>
